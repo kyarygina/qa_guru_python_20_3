d661c35a3249cd5eea75d89737a5b15cb8aa2c85
Ksenia Yarygina <kseniya.yarygina@megamarket.ru>
2025-05-28 14:22:48 +0300

    Поправлены отступы, удалено закрытие браузера из тестов

--- test_search_selenium.py
+++ test_search_selenium.py
@@ -14,17 +14,15 @@ def test_successful_search(driver_settings):
     time.sleep(2)
 
     assert 'Web results' in driver.page_source
-    driver.quit()
 
 def test_unsuccessful_search(driver_settings):
-        driver = driver_settings
-        driver.get('https://www.startpage.com/')
+    driver = driver_settings
+    driver.get('https://www.startpage.com/')
 
-        search_input = driver.find_element(By.ID, 'q')
-        search_input.send_keys('ergaegrakjnjhwebfjaw3yhya7rgfawbhe`a')
-        search_input.send_keys(Keys.RETURN)
+    search_input = driver.find_element(By.ID, 'q')
+    search_input.send_keys('ergaegrakjnjhwebfjaw3yhya7rgfawbhe`a')
+    search_input.send_keys(Keys.RETURN)
 
-        time.sleep(2)
+    time.sleep(2)
 
-        assert 'Uh-oh, there are no results for this search.' in driver.page_source
-        driver.quit()
+    assert 'Uh-oh, there are no results for this search.' in driver.page_source
